70362f365084c87181dc5ab85c559c58d49e87eb
Say my name <vuphamlethanh.public@gmail.com>
2023-05-20 04:57:59 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import {TestA, TestB} from "./test"
+
+TestA()
---
+++ src/test/test.js
@@ -0,0 +1,14 @@
+function TestA()
+{
+    console.log("testA")
+}
+
+function TestB()
+{
+    console.log("testB")
+}
+
+export {
+    TestA,
+    TestB
+}
